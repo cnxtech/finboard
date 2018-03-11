685941368c728bfd6fbfe464e83c5f794f92a0eb
Swalloow <swalloow.me@gmail.com>
2018-03-11 18:22:47 +0900

    Add airflow docker related settings

---
+++ airflow/script/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+: "${AIRFLOW__CORE__FERNET_KEY:=${FERNET_KEY:=$(python -c "from cryptography.fernet import Fernet; FERNET_KEY = Fernet.generate_key().decode(); print(FERNET_KEY)")}}"
+
+export AIRFLOW_HOME=/opt/airflow
+export AIRFLOW__CORE__SQL_ALCHEMY_CONN
+
+# Start airflow webserver
+echo "Initialize database..."
+airflow initdb
+sleep 5
+airflow webserver -p 8080
---
+++ airflow/script/restart.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+echo "Restart..."
+
+set -o errexit
+
+# Remove docker container
+if [ "$(docker ps -a | grep airflow)" ]; then
+    echo "Remove docker container"
+    docker rm -f airflow
+fi
+
+echo "Update git repository"
+pushd /home/ec2-user/Rogers-Collector/airflow
+git pull
+
+echo "Start docker container"
+docker run -it -d -p 8080:8080 --name airflow \
+    --env AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} \
+    --env AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} \
+    -v /home/ec2-user/airflow:/data:ro \
+    airflow:latest
+popd
+
+sleep 30
+echo "Start airflow scheduler"
+docker exec airflow airflow scheduler -D

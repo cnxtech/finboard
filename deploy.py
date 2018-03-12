311c08679a0f996ff853955fd126edcb99efed53
Swalloow <swalloow.me@gmail.com>
2018-03-12 21:38:45 +0900

    Add create, invoke, update lambda as args

--- deploy.py
+++ deploy.py
@@ -2,6 +2,7 @@ import os
 import pip
 import zipfile
 import boto3
+import argparse
 
 
 def pip_install(directory):
@@ -34,31 +35,75 @@ def upload_to_s3():
     s3.upload_file('collector.zip', 'rogers-collector', 'collector.zip')
 
 
+def create_lambda_function(function_name):
+    lambda_f = boto3.client('lambda', 'ap-northeast-2')
+    for func in function_name:
+        res = lambda_f.create_function(
+            FunctionName='collect_{}'.format(func),
+            Runtime='python3.6',
+            Role=os.getenv('LAMBDA_ROLE'),
+            Handler='{}.collect'.format(func),
+            Code={
+                'S3Bucket': 'rogers-collector',
+                'S3Key': 'collector.zip'
+            }
+        )
+        print(res)
+
+
+def invoke_lambda_function(function_name):
+    lambda_f = boto3.client('lambda', 'ap-northeast-2')
+    for func in function_name:
+        res = lambda_f.invoke(
+            FunctionName='collect_{}'.format(func),
+            InvocationType='RequestResponse',
+            LogType='None'
+        )
+        print(res)
+
+
 def update_lambda_function(function_name):
     lambda_f = boto3.client('lambda', 'ap-northeast-2')
     for func in function_name:
-        lambda_f.update_function_code(
-            FunctionName=func,
+        res = lambda_f.update_function_code(
+            FunctionName='collect_{}'.format(func),
             S3Bucket='rogers-collector',
-            S3Key='collector.zip',
+            S3Key='collector.zip'
         )
+        print(res)
 
 
 if __name__ == "__main__":
-    functions = ['collect_crypto']
-    lambda_dir = os.getcwd()
-    pip_install(lambda_dir)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-a", "--action", required=True)
+    parser.add_argument("-f", "--function", required=True)
 
-    # Make collector to zipfile
-    zipf = zipfile.ZipFile('collector.zip', 'w', zipfile.ZIP_DEFLATED)
-    make_zipfile(os.path.join(lambda_dir, 'collector/'), zipf)
-    zipf.close()
+    args = parser.parse_args()
+    act = args.action
+    functions = args.function.split(',')
 
-    # Append dist packages to zipfile
-    zipf = zipfile.ZipFile('collector.zip', 'a', zipfile.ZIP_DEFLATED)
-    make_zipfile(os.path.join(lambda_dir, 'dist/'), zipf)
-    zipf.close()
+    if act == 'init':
+        create_lambda_function(functions)
 
-    # Upload and update lambda function
-    upload_to_s3()
-    update_lambda_function(functions)
+    elif act == 'invoke':
+        invoke_lambda_function(functions)
+
+    elif act == 'deploy':
+        lambda_dir = os.getcwd()
+        pip_install(lambda_dir)
+
+        # Make collector to zipfile
+        zipf = zipfile.ZipFile('collector.zip', 'w', zipfile.ZIP_DEFLATED)
+        make_zipfile(os.path.join(lambda_dir, 'collector/'), zipf)
+        zipf.close()
+
+        # Append dist packages to zipfile
+        zipf = zipfile.ZipFile('collector.zip', 'a', zipfile.ZIP_DEFLATED)
+        make_zipfile(os.path.join(lambda_dir, 'dist/'), zipf)
+        zipf.close()
+
+        # Upload and update lambda function
+        upload_to_s3()
+        update_lambda_function(functions)
+
+    print("action finished!")

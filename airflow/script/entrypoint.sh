91bd30f77633bf5347a5aab3c693e5a1fa7aaa4b
Swalloow <swalloow.me@gmail.com>
2018-03-12 20:51:43 +0900

    Add airflow authentication script

--- airflow/script/entrypoint.sh
+++ airflow/script/entrypoint.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-: "${AIRFLOW__CORE__FERNET_KEY:=${FERNET_KEY:=$(python -c "from cryptography.fernet import Fernet; FERNET_KEY = Fernet.generate_key().decode(); print(FERNET_KEY)")}}"
-
 export AIRFLOW_HOME=/opt/airflow
 
 # Start airflow webserver

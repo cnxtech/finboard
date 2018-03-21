1a9145ff90ef46243f7957e59e506829a08315ec
Swalloow <swalloow.me@gmail.com>
2018-03-21 15:00:55 +0900

    Add local index test case

--- collector/test/test_index.py
+++ collector/test/test_index.py
@@ -1,7 +1,21 @@
 from index.exchange import ParserExchange
+from index.local import ParserLocal
+from index.world import ParserWorld
 
 
 def test_exchange():
     parser = ParserExchange()
     result = parser.parse()
-    assert isinstance(result, list)
+    assert len(result) != 0
+
+
+def test_local():
+    parser = ParserLocal()
+    result = parser.parse()
+    assert len(result) != 0
+
+
+def test_world():
+    parser = ParserWorld()
+    result = parser.parse()
+    assert len(result) != 0
